6f4b590d339b69d695b3dfc21d1a763ea274540a
Shahwat Hasnaine <shahwat.hasnaine@enosisbd.com>
2024-09-05 10:57:07 +0600

    Initialized

---
+++ iNavigation/iNavigation/iNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNavigationApp.swift
+//  iNavigation
+//
+//  Created by Shahwat Hasnaine on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct iNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
